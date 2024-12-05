5fbc41ff9c1e87ca40538b4104ddb4d05a10f60c
Marcos Venicio <marcos.venicio1230@hotmail.com>
2024-12-05 16:07:17 -0300

    Add table com as informações dos contatos.

--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -1,29 +1,36 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  phone: string;
+  mail: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', phone: "123456789", mail: 'H'},
+  {position: 2, name: 'Helium', phone: "123456789", mail: 'He'},
+  {position: 3, name: 'Lithium', phone: "123456789", mail: 'Li'},
+];
 
 @Component({
   selector: 'app-contact-list',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MatFormFieldModule, MatInputModule, MatTableModule],
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.scss',
 })
+
 export class ContactListComponent {
-  contactList = [
-    {
-      Name: 'Marcos Venicio',
-      Phone: '85987516774',
-      Mail: 'marcos.venicio1239@hotmail.com',
-    },
-    {
-      Name: 'Plaaaay',
-      Phone: '85987516775',
-      Mail: 'play@hotmail.com',
-    },
-    {
-      Name: 'Bruno',
-      Phone: '85917516775',
-      Mail: 'bruno@hotmail.com',
-    },
-  ];
+  displayedColumns: string[] = ['position', 'name', 'phone', 'mail'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
