61083f330143a8c2ced919b30082ccd0b20eda8b
Marcos Venicio <marcos.venicio1230@hotmail.com>
2024-12-06 21:25:54 -0300

    Criando componente create-contact e add requisição post para criação do contato

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ContactListComponent } from './components/contact-list/contact-list.component';
+import { CreateContactComponent } from './create-contact/create-contact.component';
 
 export const routes: Routes = [
   {
-    path: "",
-    component: HomeComponent
+    path: '',
+    component: HomeComponent,
   },
   {
-    path: "contacts",
-    component: ContactListComponent
-  }
+    path: 'contacts',
+    component: ContactListComponent,
+  },
+  {
+    path: 'contacts/create',
+    component: CreateContactComponent
+  },
 ];
---
+++ src/app/create-contact/create-contact.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import necessário para [(ngModel)]
+import { IContact } from '../Interfaces/contacts.interface';
+import { HttpService } from '../services/http.service';
+
+@Component({
+  selector: 'app-create-contact',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './create-contact.component.html',
+  styleUrls: ['./create-contact.component.scss'],
+})
+export class CreateContactComponent {
+  contact: IContact = {
+    phone: '',
+    mail: '',
+    name: '',
+  };
+
+  constructor(private httpService: HttpService) {}
+
+  createContact() {
+    this.httpService.createContact(this.contact).subscribe(
+      (response) => {
+        console.log('Contato criado com sucesso:', response);
+      },
+      (error) => {
+        console.error('Erro ao enviar contato:', error);
+      }
+    );
+  }
+}
